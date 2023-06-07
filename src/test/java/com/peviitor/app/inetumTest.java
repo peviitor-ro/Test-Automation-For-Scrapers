e00612c163aa0728850240eed98392b1e9d28064
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-07 10:27:35 +0300

    compare title from first job title

--- src/test/java/com/peviitor/app/inetumTest.java
+++ src/test/java/com/peviitor/app/inetumTest.java
@@ -1,21 +1,29 @@
 package com.peviitor.app;
 
-import java.lang.reflect.Array;
-import java.sql.Driver;
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.Test;
+import org.openqa.selenium.By;
+
+import java.nio.file.FileSystems;
 
 public class inetumTest {
 
     public static void main( String[] args ) throws Exception {
-        // change the path to the chromedriver when running on your machine
-        System.setProperty("webdriver.chrome.driver", "/Users/laurentiumarianbaluta/Desktop/github/Test-Automation-For-Scrapers/src/test/java/com/peviitor/app/chromedriver");
         
+        // current Path
+        Path currePath = FileSystems.getDefault().getPath("").toAbsolutePath();
+        String currentPathString = currePath.toString();
+
+        System.setProperty("webdriver.chrome.driver", currentPathString + "/src/test/java/com/peviitor/app/chromedriver");
+
         inetum.main(null);
         ObjectMapper objectMapper = new ObjectMapper();
 
@@ -25,9 +33,27 @@ public class inetumTest {
         ArrayList jobs = (ArrayList) inetumData.get("succes");
         for (Object job : jobs) {
             Map<String, Object> jobMap = (Map<String, Object>) job;
-            System.out.println(jobMap.get("job_title"));
 
-            driver.get(jobMap.get("job_link").toString());
+            try{
+                driver.get(jobMap.get("job_link").toString()); 
+                // cookie button
+
+                WebElement cookieButton = driver.findElement(By.className("agree-button"));
+                cookieButton.click();
+
+                String jobTitle = driver.findElement(By.className("card-title")).getText();
+
+                System.out.println(jobTitle);
+
+                if (jobTitle.equals(jobMap.get("job_title").toString())) {
+                    System.out.println("Job title is the same");
+                } else {
+                    System.out.println("Job title is not the same");
+                }
+                
+            } finally {
+                driver.quit();
+            }
             break;
         }
     }
