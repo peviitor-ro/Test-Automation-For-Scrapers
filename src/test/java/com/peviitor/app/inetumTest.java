ac80309adbab942c8420e13b8f24dce1276bf92b
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-08 13:18:36 +0300

    first test successfuly

--- src/test/java/com/peviitor/app/inetumTest.java
+++ src/test/java/com/peviitor/app/inetumTest.java
@@ -1,14 +1,11 @@
 package com.peviitor.app;
 
-import static org.junit.Assert.assertEquals;
-
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Map;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
@@ -31,30 +28,38 @@ public class inetumTest {
 
         Map<ArrayList, Object> inetumData = objectMapper.readValue(inetum.data.toString(), Map.class);
         ArrayList jobs = (ArrayList) inetumData.get("succes");
+        String data = "";
+
+        String apiEndpoint = "http://localhost:8000/scraper/based_scraper_py/inetum.py/";
         for (Object job : jobs) {
             Map<String, Object> jobMap = (Map<String, Object>) job;
+            
 
             try{
                 driver.get(jobMap.get("job_link").toString()); 
-                // cookie button
-
-                WebElement cookieButton = driver.findElement(By.className("agree-button"));
-                cookieButton.click();
 
                 String jobTitle = driver.findElement(By.className("card-title")).getText();
 
                 System.out.println(jobTitle);
 
                 if (jobTitle.equals(jobMap.get("job_title").toString())) {
-                    System.out.println("Job title is the same");
+                    data = "{\"" + "is_success" + "\": " + "\"" + "Pass" + "\"}";
+                    System.out.println("Pass");
                 } else {
-                    System.out.println("Job title is not the same");
-                }
-                
-            } finally {
-                driver.quit();
-            }
-            break;
+                    data = "{\"" + "is_success" + "\": " + "\"" + "Fail" + "\"" + "," + "\"" + "logs" + "\": " + "\"" + "Job title is not the same" + "\"}";
+                    System.out.println("Fail");
+                    break;
+                } 
+            } catch (Exception e) {
+                data = "{\"" + "is_success" + "\": " + "\"" + "Fail" + "\"" + "," + "\"" + "logs" + "\": " + "\"" + "Job title is not the same" + "\"}";
+                System.out.println("Fail");
+                break;
+            } 
+            // wait for 5 seconds
+            Thread.sleep(3000);
         }
+
+        driver.quit();
+        utils.makeRequest(apiEndpoint, "POST", data);
     }
 }
