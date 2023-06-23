c2c177856055048d6f060f5f328116a2a83fcc8a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-23 18:23:19 +0300

    change adi scraper test

--- src/test/java/com/peviitor/app/adiTest.java
+++ src/test/java/com/peviitor/app/adiTest.java
@@ -1,28 +1,47 @@
 package com.peviitor.app;
+import org.junit.Test;
+
 import com.google.common.base.Function;
 
 public class adiTest {
-    public static void main(String[] args) throws Exception {
-        adi.main(null);
-        String data = adi.data.toString();
+    public void compare() {
+        String data = "";
+        try{
+            adi.main(null);
+            data = adi.data.toString();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        
         String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/adi.py/";
         String companyName = "ADI";
         String careersUrl = "https://analogdevices.wd1.myworkdayjobs.com/External?q=Romania";
         String jobElementSelector = "p.css-12psxof";
         String jobTitleSelector = "h2";
-        utilsTest.initiateTest(
-            data, 
-            scraperApiEndpoint, 
-            companyName, 
-            careersUrl, 
-            jobElementSelector, 
-            jobTitleSelector, 
-            new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                String result = s.split(" ")[0];
-                return result;
-            }
-        });
+        String results = "";
+        try {
+            results = utilsTest.initiateTest(
+                data, 
+                scraperApiEndpoint, 
+                companyName, 
+                careersUrl, 
+                jobElementSelector, 
+                jobTitleSelector, 
+                new Function<String, String>() {
+                @Override
+                public String apply(String s) {
+                    String result = s.split(" ")[0];
+                    return result;
+                }
+            });
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    @Test
+    public void test() {
+        compare();
     }
 }
