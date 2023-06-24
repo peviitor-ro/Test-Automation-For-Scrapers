e24a0dbd9fe57f2dddf3f7b85670ba36e3f4d9cc
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-24 20:17:40 +0300

    change adp scraper test

--- src/test/java/com/peviitor/app/adpTest.java
+++ src/test/java/com/peviitor/app/adpTest.java
@@ -1,28 +1,51 @@
 package com.peviitor.app;
+
+import org.junit.Test;
+import org.testng.asserts.Assertion;
 import com.google.common.base.Function;
 
 public class adpTest {
-    public static void main(String[] args) throws Exception {
-        adp.main(null);
-        String data = adp.data.toString();
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+        try {
+            adp.main(null);
+            data = adp.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
         String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/adp.py/";
         String companyName = "ADP";
         String careersUrl = "https://jobs.adp.com/job-search-results/?language=en&location=Romania&country=RO&radius=25";
         String jobElementSelector = "span[id='live-results-counter']";
         String jobTitleSelector = "title";
-        utilsTest.initiateTest(
-            data, 
-            scraperApiEndpoint, 
-            companyName, careersUrl, 
-            jobElementSelector, 
-            jobTitleSelector,
-            null,
-            new Function<String, String>() {
-            @Override
-            public String apply(String s) {
-                String result = s.split(",")[0];
-                return result;
-            }
-        });
+
+        try {
+            results = utilsTest.initiateTest(
+                        data,
+                        scraperApiEndpoint,
+                        companyName, careersUrl,
+                        jobElementSelector,
+                        jobTitleSelector,
+                        null,
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                String result = s.split(",")[0];
+                                return result;
+                            }
+                        });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
     }
 }
