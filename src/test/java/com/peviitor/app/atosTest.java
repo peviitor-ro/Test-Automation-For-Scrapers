b6ed0ea4cf77b08c9bfce13211123d0c944380e9
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-25 20:35:46 +0300

    create test for auchan scraper

---
+++ src/test/java/com/peviitor/app/atosTest.java
@@ -0,0 +1,60 @@
+package com.peviitor.app;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+import com.google.common.base.Function;
+
+public class atosTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+        try {
+            atos.main(null);
+            data = atos.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Atos";
+        String careersUrl = "https://jobs.atos.net/go/Jobs-in-Romania/3686501/0/?q=&sortColumn=referencedate&sortDirection=desc";
+        String jobElementSelector = "span[class='paginationLabel']";
+        String jobTitleSelector = "h1";
+
+        try {
+            results = utilsTest.initiateTest(
+                        data,
+                        scraperApiEndpoint,
+                        companyName, careersUrl,
+                        jobElementSelector,
+                        jobTitleSelector,
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                ArrayList<String> result = new ArrayList<String>(){{
+                                    for (String i : s.split(" ")) {
+                                        add(i);
+                                    }
+                                }};
+                                return result.get(result.size() - 1);
+                            }
+                        },
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                String result = s.replace("  ", " ");
+                                return result;
+                            }
+                        });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
