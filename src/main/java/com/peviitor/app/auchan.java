6322d87d772a40e9771cc6d5e332233532140861
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-26 10:03:17 +0300

    create test for auchan scraper

---
+++ src/main/java/com/peviitor/app/auchan.java
@@ -0,0 +1,11 @@
+package com.peviitor.app;
+
+public class auchan {
+    public static String pythonScrapersUrl = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/";
+    public static String dataObject = '{' + "\"file\": " + "\"auchan.py\"" + '}';
+    public static Object data = null;
+
+    public static void main(String[] args) throws Exception {
+        data = utils.makeRequest(pythonScrapersUrl, "POST", dataObject);
+    }
+}
---
+++ src/test/java/com/peviitor/app/auchanTest.java
@@ -0,0 +1,60 @@
+package com.peviitor.app;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+import com.google.common.base.Function;
+
+public class auchanTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+        try {
+            auchan.main(null);
+            data = auchan.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Auchan";
+        String careersUrl = "https://cariere.auchan.ro";
+        String jobElementSelector = "span[class='k-pager-info k-label']";
+        String jobTitleSelector = "h1";
+
+        try {
+            results = utilsTest.initiateTest(
+                        data,
+                        scraperApiEndpoint,
+                        companyName, careersUrl,
+                        jobElementSelector,
+                        jobTitleSelector,
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                ArrayList<String> result = new ArrayList<String>(){{
+                                    for (String i : s.split(" ")) {
+                                        add(i);
+                                    }
+                                }};
+                                return result.get(result.size() - 2);
+                            }
+                        },
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                String result = s.replace(" ", "");
+                                return result;
+                            }
+                        });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
