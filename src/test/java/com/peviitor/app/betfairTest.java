d262f5faed5353e6e315f18b5c5eda24c91a266d
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-28 00:06:16 +0300

    create tests for bcr, betfair and cargill scrapers

---
+++ src/test/java/com/peviitor/app/betfairTest.java
@@ -0,0 +1,60 @@
+package com.peviitor.app;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+import com.google.common.base.Function;
+
+public class betfairTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+        try {
+            betfair.main(null);
+            data = betfair.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Betfair";
+        String careersUrl = "https://www.betfairromania.ro/find-a-job/?search=&country=Romania&pagesize=1000#results";
+        String jobElementSelector = "p[class='job-count']";
+        String jobTitleSelector = "h2";
+
+        try {
+            results = utilsTest.initiateTest(
+                        data,
+                        scraperApiEndpoint,
+                        companyName, careersUrl,
+                        jobElementSelector,
+                        jobTitleSelector,
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                ArrayList<String> result = new ArrayList<String>(){{
+                                    for (String i : s.split(" ")) {
+                                        add(i);
+                                    }
+                                }};
+                                return result.get(result.size() - 3);
+                            }
+                        },
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                String result = s.toUpperCase();
+                                return result;
+                            }
+                        });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
---
+++ src/test/java/com/peviitor/app/cargillTest.java
@@ -0,0 +1,48 @@
+package com.peviitor.app;
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+import com.google.common.base.Function;
+
+public class cargillTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+        try {
+            cargill.main(null);
+            data = cargill.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Cargill";
+        String careersUrl = "https://careers.cargill.com/en/search-jobs/Romania/23251/2/798549/46/25/50/2";
+        String jobElementSelector = "section[id='search-results']";
+        String jobTitleSelector = "div[class='jd-text-info']";
+
+        try {
+            results = utilsTest.initiateTest(
+                        data,
+                        scraperApiEndpoint,
+                        companyName, careersUrl,
+                        jobElementSelector,
+                        jobTitleSelector,
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                String result = s.split(" ")[0];
+                                return result;
+                            }
+                        });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
