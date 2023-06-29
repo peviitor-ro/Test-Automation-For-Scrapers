9acf9d6cafa19f428549ca88bc6b4fb74e987054
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-30 00:34:58 +0300

    create test for danone scraper

---
+++ src/test/java/com/peviitor/app/danoneTest.java
@@ -0,0 +1,57 @@
+package com.peviitor.app;
+import java.util.ArrayList;
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+import com.google.common.base.Function;
+
+public class danoneTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+
+        try {
+            danone.main(null);
+            data = danone.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Danone";
+        String careersUrl = "https://careers.danone.com/en-global/jobs.html?countries=Romania";
+        String jobElementSelector = "span[class='dn-job-card-count__item']";
+        String jobTitleSelector = "h1";
+
+        try {
+            results = utilsTest.initiateTest(
+                        data,
+                        scraperApiEndpoint,
+                        companyName, careersUrl,
+                        jobElementSelector,
+                        jobTitleSelector,
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                return s;
+                            }
+                        },
+                        new Function<String, String>() {
+                            @Override
+                            public String apply(String s) {
+                                String result = s.toUpperCase();
+                                return result;
+                            }
+                        });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
