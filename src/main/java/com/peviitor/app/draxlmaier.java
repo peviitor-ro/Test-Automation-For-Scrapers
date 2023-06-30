31dd5eb05f2dd554c9659d10252ea1a3642ae1ec
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-01 00:10:49 +0300

    create test for draxlmaier scraper

---
+++ src/main/java/com/peviitor/app/draxlmaier.java
@@ -0,0 +1,11 @@
+package com.peviitor.app;
+
+public class draxlmaier {
+    public static String pythonScrapersUrl = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/";
+    public static String dataObject = '{' + "\"file\": " + "\"draxlmaier.py\"" + '}';
+    public static Object data = null;
+
+    public static void main(String[] args) throws Exception {
+        data = utils.makeRequest(pythonScrapersUrl, "POST", dataObject);
+    }
+}
