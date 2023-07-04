15cd5bc0edc168adbab30ec1d79156f1b84d4319
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-04 16:46:53 +0300

    create test for kelloggs scraper

---
+++ src/test/java/com/peviitor/app/kelloggsTest.java
@@ -0,0 +1,67 @@
+package com.peviitor.app;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+
+import com.google.common.base.Function;
+
+public class kelloggsTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+
+        try {
+            kelloggs.main(null);
+            data = kelloggs.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Kelloggs";
+        String careersUrl = "https://jobs.kellogg.com/search/?createNewAlert=false&q=&locationsearch=Romania";
+        String jobElementSelector = "span[class='paginationLabel']";
+        String jobTitleSelector = "h1";
+
+        try {
+            results = new utilsTest().initiateTest(
+                    data,
+                    scraperApiEndpoint,
+                    companyName, careersUrl,
+                    jobElementSelector,
+                    jobTitleSelector,
+                    new Function<String, String>() {
+                        @Override
+                        public String apply(String s) {
+                            ArrayList<String> result = new ArrayList<String>() {
+                                {
+                                    for (String i : s.split(" ")) {
+                                        add(i);
+                                    }
+                                }
+                            };
+                            return result.get(result.size() - 1);
+                        }
+                    },
+                    new Function<String, String>() {
+                        @Override
+                        public String apply(String s) {
+                            return s.replace("Title: ", "");
+                        }
+                    }
+                    );
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
