054ddc5a407bf9fbd7ffd55b6e750810f68b5e27
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-04 16:45:54 +0300

    create test for intel scraper

---
+++ src/main/java/com/peviitor/app/intel.java
@@ -0,0 +1,11 @@
+package com.peviitor.app;
+
+public class intel {
+    public static String pythonScrapersUrl = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/";
+    public static String dataObject = '{' + "\"file\": " + "\"intel.py\"" + '}';
+    public static Object data = null;
+
+    public static void main(String[] args) throws Exception {
+        data = utils.makeRequest(pythonScrapersUrl, "POST", dataObject);
+    }
+}
---
+++ src/test/java/com/peviitor/app/intelTest.java
@@ -0,0 +1,51 @@
+package com.peviitor.app;
+
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+
+import com.google.common.base.Function;
+
+public class intelTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+
+        try {
+            intel.main(null);
+            data = intel.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/astrazeneca.py/";
+        String companyName = "Intel";
+        String careersUrl = "https://jobs.intel.com/en/search-jobs/Romania/599/2/798549/46/25/50/2";
+        String jobElementSelector = "#search-results > h1";
+        String jobTitleSelector = "h1";
+
+        try {
+            results = new utilsTest().initiateTest(
+                    data,
+                    scraperApiEndpoint,
+                    companyName, careersUrl,
+                    jobElementSelector,
+                    jobTitleSelector,
+                    new Function<String, String>() {
+                        @Override
+                        public String apply(String s) {
+                            return s.split(" ")[2];
+                        }
+                    });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
