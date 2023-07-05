734b917951dad4fccbe63930ec4d1387d1ca87a0
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-05 06:43:46 +0300

    create test for mazars

---
+++ src/test/java/com/peviitor/app/mazarsTest.java
@@ -0,0 +1,121 @@
+package com.peviitor.app;
+
+import org.junit.Test;
+import org.testng.asserts.Assertion;
+
+import com.google.common.base.Function;
+import java.util.ArrayList;
+import java.util.Map;
+
+class mazarsUtils extends utilsTest {
+    @Override
+    public String initiateTest(
+            String appData,
+            String scraperApiEndpoint,
+            String companyName,
+            String careersUrl,
+            // this selectors must be visible on the careers page
+            String jobElementSelector,
+            String jobTitleSelector,
+            Function<String, String>... callFunctions) throws Exception {
+
+        System.out.println("Testing " + companyName + " scraper...");
+
+        String apiEndpoint = scraperApiEndpoint;
+
+        Boolean firstTestResult = checkNumberOfJobs(appData, companyName, careersUrl, jobElementSelector, null); // first test is true because the number of jobs is not displayed on the
+                                        // careers page
+
+        // check if the number of jobs is the same
+        if (firstTestResult) {
+            // if the number of jobs is the same, then the test is passed
+            System.out.println("First test passed");
+
+            Boolean secondTestResult = false;
+            if (callFunctions.length >= 2) {
+                secondTestResult = checkLink(jobTitleSelector, callFunctions[1]);
+            } else {
+                secondTestResult = checkLink(jobTitleSelector, null);
+            }
+
+            if (secondTestResult) {
+                System.out.println("Second test passed");
+
+            } else {
+                System.out.println("Second test failed");
+            }
+
+        } else { // if the number of jobs is not the same
+            System.out.println("First Test failed!");
+            System.out.println("Peviitor: " + peviitorJobs);
+            System.out.println("Scraper: " + scraperJobs);
+            System.out.println("Career page: " + careerPageJobs);
+
+            data = "{\"" + "is_success" + "\": " + "\"" + "Fail" + "\"" + "," + "\"" + "logs" + "\": " + "\""
+                    + "Number of jobs is not the same" + "\"}";
+            testResult = "false";
+        }
+        // // make request to the api to save the test result
+        // utils.makeRequest(apiEndpoint, "POST", data);
+        return testResult;
+    };
+
+    @Override
+    public Boolean checkNumberOfJobs(
+            String appData,
+            String companyName,
+            String careersUrl,
+            String jobElementSelector,
+            Function<String, String> callFunction) throws Exception {
+
+        Map<ArrayList, Object> scraperData = objectMapper.readValue(appData.toString(), Map.class);
+        jobs = (ArrayList) scraperData.get("succes");   
+        return true;
+    };
+}
+
+public class mazarsTest {
+    public static String results = "";
+
+    public void compare() {
+        String data = "";
+
+        try {
+            mazars.main(null);
+            data = mazars.data.toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        String scraperApiEndpoint = "https://dev.laurentiumarian.ro/scraper/based_scraper_py/mazars.py/";
+        String companyName = "Mazars";
+        String careersUrl = null;
+        String jobElementSelector = null;
+        String jobTitleSelector = ".Breadcrumb-current";
+
+        try {
+            results = new mazarsUtils().initiateTest(
+                    data,
+                    scraperApiEndpoint,
+                    companyName, careersUrl,
+                    jobElementSelector,
+                    jobTitleSelector,
+                    null,
+                    new Function<String, String>() {
+                        @Override
+                        public String apply(String s) {
+                            return s.replace(" ", "");
+                        }
+                    });
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Test
+    public void test() {
+        compare();
+        Assertion assertion = new Assertion();
+        assertion.assertEquals(results, "true");
+    }
+}
